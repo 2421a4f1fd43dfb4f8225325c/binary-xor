3beabe43301153fd5a93d4f002a0b7b7130d5b14
2421a4f1fd43dfb4f8225325c <73066585+2421a4f1fd43dfb4f8225325c@users.noreply.github.com>
2020-10-23 09:43:29 +0200

    Update xor-set-key.sh

--- xor-set-key.sh
+++ xor-set-key.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 enc=""
 data=""
+key_svd=""
 read -e -r -p "text: " data
-read -e -r -p "key: " key
-key_length="$(echo $key | wc -c)"
 data="$(echo $data | perl -lpe '$_=unpack"B*"')"
-key="$(echo $key | perl -lpe '$_=unpack"B*"')"
-key=( $(echo $key | sed 's/.\{1\}/& /g') )
 data="$(echo $data | sed 's/.\{1\}/& /g')"
 echo $data
-key_location=0
 for i in $data;do
-case $i${key[$key_location]} in
+key="$((RANDOM%2))"
+key_svd="$key_svd$key"
+case $i$key in
     00|11)
     enc+=0
     ;;
     01|10)
     enc+=1
     ;;
-    *)
-    echo $i${key[$key_location]}
-    ;;
 esac
-((key_location++))
-if [ "$key_location" -gt "$key_length" ];then
-key_location=0
-fi
 done
 clear
 echo -n "Ciphertext: "
 echo "$enc" | sed 's/.\{8\}/& /g'
 echo -n "Key: "
 echo "$key_svd" | sed 's/.\{8\}/& /g'
-
-echo "ibase=2;obase=G;$enc" | bc
+echo "ibase=2;obase=G;$enc" | bc | sed 's/.\{2\}/& /g'
 echo "ibase=2;obase=G;$enc" | bc | xxd -p -r
 echo "ibase=2;obase=G;$enc" | bc | xxd -p -r > encrypted
+echo
+echo "ibase=2;obase=G;$key_svd" | bc
+echo "ibase=2;obase=G;$key_svd" | bc | xxd -p -r
+echo "ibase=2;obase=G;$key_svd" | bc | xxd -p -r > key
