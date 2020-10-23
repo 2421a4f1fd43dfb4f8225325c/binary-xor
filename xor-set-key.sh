bf92469331a1ce7ae4906a9362f0e3bbe50d57cb
2421a4f1fd43dfb4f8225325c <73066585+2421a4f1fd43dfb4f8225325c@users.noreply.github.com>
2020-10-23 08:11:34 +0200

    Add files via upload

---
+++ xor-set-key.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+enc=""
+data=""
+read -e -r -p "text: " data
+read -e -r -p "key: " key
+data="$(echo $data | perl -lpe '$_=unpack"B*"')"
+key="$(echo $key | perl -lpe '$_=unpack"B*"')"
+key=( $(echo $key | sed 's/.\{1\}/& /g') )
+data="$(echo $data | sed 's/.\{1\}/& /g')"
+echo $data
+key_location=0
+for i in $data;do
+case $i${key[$key_location]} in
+    00|11)
+    enc+=0
+    ;;
+    01|10)
+    enc+=1
+    ;;
+    *)
+    echo $i${key[$key_location]}
+    ;;
+esac
+((key_location++))
+done
+#clear
+echo -n "Ciphertext: "
+echo "$enc" | sed 's/.\{8\}/& /g'
+echo -n "Key: "
+echo "$key_svd" | sed 's/.\{8\}/& /g'
+
+echo "ibase=2;obase=G;$enc" | bc
+echo "ibase=2;obase=G;$enc" | bc | xxd -p -r
+echo "ibase=2;obase=G;$enc" | bc | xxd -p -r > encrypted
