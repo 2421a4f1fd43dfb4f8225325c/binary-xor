afa4851ea39890774f06a0ab8c0baf18ae6bdf70
2421a4f1fd43dfb4f8225325c <73066585+2421a4f1fd43dfb4f8225325c@users.noreply.github.com>
2020-11-14 05:36:53 +0100

    Update xor.sh

--- xor.sh
+++ xor.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-getrandom(){
-urandom="$(cat /dev/urandom | xxd -u -l 1 -p)"
-echo "ibase=G;obase=2;$urandom" | bc | cut -c 6-6
-}
-getrandom
 enc=""
 data=""
-keyTotal=""
+key_svd=""
 read -e -r -p "text: " data
 data="$(echo $data | perl -lpe '$_=unpack"B*"')"
 data="$(echo $data | sed 's/.\{1\}/& /g')"
 echo $data
 for i in $data;do
-key="$(getrandom)"
-keyTotal="$keyTotal$key"
+key="$((RANDOM%2))"
+key_svd="$key_svd$key"
 case $i$key in
     00|11)
     enc+=0
@@ -27,7 +22,7 @@ clear
 echo -n "Ciphertext: "
 echo "$enc" | sed 's/.\{8\}/& /g'
 echo -n "Key: "
-echo "$keyTotal" | sed 's/.\{8\}/& /g'
+echo "$key_svd" | sed 's/.\{8\}/& /g'
 for i in $(echo "ibase=2;obase=G;$enc" | bc | sed 's/.\{2\}/& /g');do
 i="${i//\\/}"
 i="${i// /}"
@@ -39,11 +34,11 @@ echo "ibase=2;obase=G;$enc" | bc | xxd -p -r > encrypted
 for i in $(seq $(tput cols));do
 echo -n "#"
 done
-for i in $(echo "ibase=2;obase=G;$keyTotal" | bc | sed 's/.\{2\}/& /g');do
+for i in $(echo "ibase=2;obase=G;$key_svd" | bc | sed 's/.\{2\}/& /g');do
 i="${i//\\/}"
 i="${i// /}"
 for x in $i;do
 echo -n "${x} "
 done
 done;echo
-echo "ibase=2;obase=G;$keyTotal" | bc | xxd -p -r > key
+echo "ibase=2;obase=G;$key_svd" | bc | xxd -p -r > key
