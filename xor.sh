bba11eee852685f755821fd312454cd3acef696c
2421a4f1fd43dfb4f8225325c <73066585+2421a4f1fd43dfb4f8225325c@users.noreply.github.com>
2020-12-05 05:51:52 -0500

    Update xor.sh

--- xor.sh
+++ xor.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-rng(){
-    cat /dev/urandom|hexdump -v -e '/1 "%u"' -n 1
-}
 enc="";data="";key_svd=""
-data="$(cat /dev/stdin)";echo
-data="$(echo $data|perl -lpe '$_=unpack"B*"'|sed 's/.\{1\}/& /g')"
-echo $data
+data="$(cat|perl -lpe '$_=unpack"B*"'|sed 's/.\{1\}/& /g')"
+printf "\n$data\n"
 for i in $data;do
-	key="$(rng)"
+	key=$(cat /dev/urandom|hexdump -v -e '/1 "%u"' -n 1)
 	enc+=$(((i+(key%=2))%2))
 	key_svd+=$key
 done
@@ -15,8 +11,8 @@ clear
 echo "Ciphertext: `echo "$enc"|sed 's/.\{8\}/& /g'`"
 echo "Key: `echo "$key_svd"|sed 's/.\{8\}/& /g'`"
 for i in $(seq $(tput cols));do echo -n "#";done;echo
-echo "ibase=2;obase=G;$enc"|bc|sed 's/.\{2\}/& /g'|sed 's/\\//g'
-for i in $(seq $(tput cols));do echo -n "#";done
-echo "ibase=2;obase=G;$key_svd"|bc|sed 's/.\{2\}/& /g'|sed 's/\\//g'
-echo "ibase=2;obase=G;$key_svd"|bc|xxd -p -r > key
 echo "ibase=2;obase=G;$enc"|bc|xxd -p -r > encrypted
+cat encrypted|xxd -u -l 10000000000 -p|sed 's/.\{2\}/& /g'|tr -d '\n';echo
+for i in $(seq $(tput cols));do echo -n "#";done
+echo "ibase=2;obase=G;$key_svd"|bc|xxd -p -r > key
+cat key|xxd -u -l 10000000000000000 -p|sed 's/.\{2\}/& /g'|tr -d '\n';echo
